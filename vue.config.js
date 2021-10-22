befff4c2bd517d178d7b938a4dcc09deb094e302
kingyinliang <17600813147@163.com>
2021-10-22 12:55:13 +0800

    feat(img): add image-webpack-loader

--- vue.config.js
+++ vue.config.js
@@ -30,5 +30,16 @@ module.exports = {
     disableHostCheck: true
   },
   pages,
-  configureWebpack
+  configureWebpack,
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        bypassOnDebug: true
+      })
+      .end()
+  }
 }
